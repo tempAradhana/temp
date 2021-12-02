bd97a6057c6564dbc6dec041a22fded681826ed9
tempAradhana <95422440+tempAradhana@users.noreply.github.com>
2021-12-02 18:40:52 +0530

    Update First.java

--- First.java
+++ First.java
@@ -1,7 +1,9 @@
 class First{  
     public static void main(String args[]){  
         int a;
-        a ++;
+        a++;
+        int b;
+        b--;
      System.out.println("Hello Java");  
      System.out.println("Creating PR1");
     }  
